5621e4272f07249dddf36656287dac575dcc86fb
kiki <15904306959@163.com>
2024-08-12 18:48:09 +0800

    master commit2

--- src/main/java/com/lhr/git/GitTest.java
+++ src/main/java/com/lhr/git/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
     public static void main(String[] args){
         System.out.println("hello git!");
         System.out.println("hello git!");
-        System.out.println("hello git!");
+        System.out.println("hello git!4");
+        System.out.println("master test!");
     }
 }
